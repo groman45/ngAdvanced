ba925fc57af261705e56339d313806dfbe9eda4a
groman45 <groman45@gmail.com>
2024-06-02 12:18:59 +0300

    Add filtering control

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,48 +2,48 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } fr
 import { CommonModule } from '@angular/common';
 import { PostsService } from './posts.service'
 import { IPost } from './models/post';
-import { filter, from, map, Observable, Subscription, tap} from "rxjs";
+import { filter, from, map, Observable, Subscription, tap, mergeMap } from "rxjs";
 import { FormControl } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterComponent } from '../shared/filter.component'
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [CommonModule, FormsModule,   ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, FilterComponent],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent implements OnInit, OnDestroy {
-  
-  public searchControl: FormControl = new FormControl(''); // Filter
+export class PostsComponent implements OnInit, OnDestroy {  
+
   posts: IPost[] | undefined;
-  subscription: Subscription| undefined;  
+  subscription: Subscription | undefined;  
 
   constructor(private postsService: PostsService) {
   }
 
   ngOnInit(): void {   
-    this.getDataFromService('');      
-    this.searchControl.valueChanges
-            .subscribe(query => {                
-              this.getDataFromService(query);
-        });
-    
+    this.getDataFromService('', 0);   
   }
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();     
   }
 
-  getDataFromService(srcTxt: string) {
-    //this.postsService.getPosts().subscribe({
-    //  next: (posts: IPost[]) => { this.posts = posts; },
-    //  error: (e) => { console.error(e); }
-   // });
+  getDataFromService(srcTxt: string, numberOfItems: number) {   
 
     this.subscription?.unsubscribe();
-    this.subscription = this.postsService.getPosts().pipe(
-      map(items => items.filter(i => srcTxt == "" || i.id.toString() == srcTxt || i.title.includes(srcTxt) || i.body.includes(srcTxt))))
+    
+    let observable$ = this.postsService.getPosts();
+    if(srcTxt && srcTxt.length > 0) // Append filter
+      observable$ = observable$.pipe(
+          map(items => items.filter(i => i.id.toString() == srcTxt 
+                || i.title.includes(srcTxt) || i.body.includes(srcTxt)))
+              );
+    if(numberOfItems > 0)
+      observable$ = observable$.pipe(map(items => items.slice(0, numberOfItems)));
+    
+    this.subscription = observable$
       .subscribe({
         next: (posts: IPost[]) => { this.posts = posts; },
         error: (e) => { console.error(e); }
---
+++ src/app/shared/filter.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+export interface FilterComponentEventArgs {
+  filterTxt: string;
+  numberOfItems: number;
+}
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [CommonModule, FormsModule,  ReactiveFormsModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.scss'
+})
+export class FilterComponent implements OnInit{
+
+  eventArgs: FilterComponentEventArgs = { filterTxt:"", numberOfItems:0 };
+  searchControl: FormControl = new FormControl('');  // Filter Control
+  @Output() FilterChanged = new EventEmitter<FilterComponentEventArgs>();
+
+  ngOnInit(): void {   
+     
+    this.searchControl.valueChanges
+            .subscribe(filterTxt => {    
+              this.eventArgs.filterTxt = filterTxt;          
+              this.FilterChanged.emit(this.eventArgs);
+        });    
+  }
+
+  onNumItemsSelected(newValue: string) {
+    this.eventArgs.numberOfItems = +newValue;  
+    this.FilterChanged.emit(this.eventArgs);
+ }
+}
